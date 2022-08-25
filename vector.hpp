3cb738f24f22ce5e7869213e833053336e689041
grvelva <grvelva@student.42.fr>
2022-08-25 14:27:52 +0200

    simple vector with std::allocator

--- vector.hpp
+++ vector.hpp
@@ -15,21 +15,23 @@ namespace ft {
 		std::allocator<T> _allocator;
     public:
         explicit vector(const std::allocator<T>& allocator = std::allocator<T>()) {
-            _arr = new T[BASIC_SIZE];
 			_index = 0;
-            _size = BASIC_SIZE;
+			_size = BASIC_SIZE;
 			_allocator = allocator;
-        };
+			_arr = _allocator.allocate(BASIC_SIZE);
+		};
 
-        explicit vector(unsigned int size) {
+        explicit vector(unsigned int size, const std::allocator<T>& allocator = std::allocator<T>()) {
             size = (BASIC_SIZE > size) ? BASIC_SIZE : size;
-            _arr = new T[size];
             _size = size;
 			_index = 0;
-        };
+			_allocator = allocator;
+			_arr = _allocator.allocate(size);
+		};
 
-        vector(vector const &other) {
-            _arr = new T[other._size];
+        vector(vector const &other, const std::allocator<T>& allocator = std::allocator<T>()) {
+			_allocator = allocator;
+            _arr = _allocator.allocate(other._size);
             for (unsigned int i = 0; i < other._size; ++i)
                 _arr[i] = other._arr[i];
             _size = other._size;
@@ -37,15 +39,15 @@ namespace ft {
         }
 
         ~vector() {
-            delete[] _arr;
+            _allocator.deallocate(_arr, _size);
         }
 
         vector &operator=(const vector &other) {
             if (this == &other)
                 return (*this);
             if (_size < other._size) {
-                delete[] _arr;
-                _arr = new T[other._size];
+                _allocator.deallocate(_arr, _size);
+                _arr = _allocator.allocate(other._size);
             }
             for (unsigned int i = 0; i < _size; ++i)
                 _arr[i] = other._arr[i];
@@ -62,12 +64,12 @@ namespace ft {
         void push_back(T new_el){
             if (_index + 1 >= _size)
             {
-                T *new_arr = new T[_size << 1];
+                T *new_arr = _allocator.allocate(_size << 1);
                 for (unsigned int i = 0; i < _size; ++i) {
                     new_arr[i] = _arr[i];
                 }
-                _size = _size << 1;
-                delete[] _arr;
+				_allocator.deallocate(_arr, _size);
+				_size = _size << 1;
                 _arr = new_arr;
             }
             _arr[_index++] = new_el;
