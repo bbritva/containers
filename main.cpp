370fab7640cd96bba8667247583c344ac8c38561
grvelva <grvelva@student.42.fr>
2022-09-14 17:02:31 +0200

    simple stack on std::vector

--- main.cpp
+++ main.cpp
@@ -2,52 +2,71 @@
 // Created by grvelva on 8/25/22.
 //
 #include <vector>
-//    #define VECTOR std::vector
-#define VECTOR ft::vector
+#include <stack>
+#define NAMESPACE ft
+//#define NAMESPACE std
 #include "vector.hpp"
+#include "stack.hpp"
 
 #include <cstdlib>
 
 #define COUNT 100
 
 int main() {
-	VECTOR<std::string> vector_str;
-	VECTOR<int> vector_int;
+	/*{ //VECTOR
+		NAMESPACE::vector<std::string> vector_str;
+		NAMESPACE::vector<int> vector_int;
 
-	for (int i = 0; i < COUNT; i++)
-	{
-		vector_int.push_back(i);
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		vector_int.pop_back();
-	}
-	for (unsigned int i = 0; i < vector_int.size(); i++)
-	{
-		std::cout << vector_int[i];
+		for (int i = 0; i < COUNT; i++) {
+			vector_int.push_back(i);
+		}
+		for (int i = 0; i < 5; i++) {
+			vector_int.pop_back();
+		}
+		for (unsigned int i = 0; i < vector_int.size(); i++) {
+			std::cout << vector_int[i];
+		}
+
+		for (int i = 0; i < COUNT; i++) {
+			vector_str.push_back("sw");
+		}
+		for (int i = 0; i < 5; i++) {
+			vector_str.pop_back();
+		}
+		for (unsigned int i = 0; i < vector_str.size(); i++) {
+			std::cout << vector_str[i];
+		}
+		std::cout << std::endl;
+	}*/
+	{ //STACK
+		NAMESPACE::stack<std::string> stack_str;
+		NAMESPACE::stack<int> stack_int;
+
+		for (int i = 0; i < COUNT; i++) {
+			stack_int.push(i);
+		}
+		for (int i = 0; i < 5; i++) {
+			stack_int.pop();
+		}
+		while (!stack_int.empty()) {
+			std::cout << stack_int.top();
+			stack_int.pop();
+		}
+
+		for (int i = 0; i < COUNT; i++) {
+			stack_str.push("i");
+		}
+		for (int i = 0; i < 5; i++) {
+			stack_str.pop();
+		}
+		while (!stack_str.empty()) {
+			std::cout << stack_str.top();
+			stack_str.pop();
+		}
+		std::cout << std::endl;
 	}
 
-	for (int i = 0; i < COUNT; i++)
-	{
-		vector_str.push_back("sw");
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		vector_str.pop_back();
-	}
-	for (unsigned int i = 0; i < vector_str.size(); i++)
-	{
-		std::cout << vector_str[i];
-	}
-	std::cout << std::endl;
-	std::vector<int> std_vector;
-	for (int i = 0; i < COUNT; i++)
-	{
-		std_vector.push_back(i);
-	}
-	ft::vector<int>::iterator it = vector_str.begin();
-	while (it != std_vector.end())
-		std::cout << *it++;
+
 
 
 	return (0);
---
+++ stack.hpp
@@ -0,0 +1,40 @@
+//
+// Created by grvelva on 9/14/22.
+//
+
+#ifndef CONTAINERS_STACK_HPP
+#define CONTAINERS_STACK_HPP
+
+namespace ft {
+	template<typename T, class Container = std::vector<T> >
+	class stack {
+	private:
+		Container _data;
+
+	public:
+		explicit stack(const Container& cont = Container()) : _data(cont){
+		};
+
+		T& top() {
+			return _data.back();
+		}
+
+		bool empty() const {
+			return _data.empty();
+		}
+
+		std::size_t size() const {
+			return _data.size();
+		}
+
+		void push(const T element) {
+			_data.push_back(element);
+		}
+
+		void pop() {
+			_data.pop_back();
+		}
+	};
+}
+
+#endif //CONTAINERS_STACK_HPP
