be39580880504c9860246c744ec4d91ad11f2463
Boris <amgboris@ya.ru>
2022-09-16 09:04:49 +0200

    tabs

--- vector.hpp
+++ vector.hpp
@@ -6,62 +6,62 @@
 # include <iostream>
 
 namespace ft {
-    template<typename T>
-    class vector {
-    private:
-        T *_arr;
-        std::size_t _capacity;
-        std::size_t _size;
+	template<typename T>
+	class vector {
+	private:
+		T *_arr;
+		std::size_t _capacity;
+		std::size_t _size;
 		std::allocator<T> _allocator;
-    public:
-        explicit vector(const std::allocator<T>& allocator = std::allocator<T>()) {
+	public:
+		explicit vector(const std::allocator<T>& allocator = std::allocator<T>()) {
 			_size = 0;
 			_capacity = BASIC_SIZE;
 			_allocator = allocator;
 			_arr = _allocator.allocate(BASIC_SIZE);
 		};
 
-        explicit vector(std::size_t size, const std::allocator<T>& allocator = std::allocator<T>()) {
-            size = (BASIC_SIZE > size) ? BASIC_SIZE : size;
+		explicit vector(std::size_t size, const std::allocator<T>& allocator = std::allocator<T>()) {
+			size = (BASIC_SIZE > size) ? BASIC_SIZE : size;
 			_capacity = size;
 			_size = 0;
 			_allocator = allocator;
 			_arr = _allocator.allocate(size);
 		};
 
-        vector(vector const &other, const std::allocator<T>& allocator = std::allocator<T>()) {
+		vector(vector const &other, const std::allocator<T>& allocator = std::allocator<T>()) {
 			_allocator = allocator;
-            _arr = _allocator.allocate(other._capacity);
-            for (std::size_t i = 0; i < other._capacity; ++i)
-                _arr[i] = other._arr[i];
+			_arr = _allocator.allocate(other._capacity);
+			for (std::size_t i = 0; i < other._capacity; ++i)
+				_arr[i] = other._arr[i];
 			_capacity = other._capacity;
 			_size = other._size;
-        }
+		}
 
-        ~vector() {
-            _allocator.deallocate(_arr, _capacity);
-        }
+		~vector() {
+			_allocator.deallocate(_arr, _capacity);
+		}
 
-        vector &operator=(const vector &other) {
-            if (this == &other)
-                return (*this);
-            if (_capacity < other._capacity) {
-                _allocator.deallocate(_arr, _capacity);
-                _arr = _allocator.allocate(other._capacity);
-            }
-            for (std::size_t i = 0; i < _capacity; ++i)
-                _arr[i] = other._arr[i];
+		vector &operator=(const vector &other) {
+			if (this == &other)
+				return (*this);
+			if (_capacity < other._capacity) {
+				_allocator.deallocate(_arr, _capacity);
+				_arr = _allocator.allocate(other._capacity);
+			}
+			for (std::size_t i = 0; i < _capacity; ++i)
+				_arr[i] = other._arr[i];
 			_capacity = other._capacity;
-            return (*this);
-        }
+			return (*this);
+		}
 
 		const std::allocator<T>& get_allocator() const {
 			return _allocator;
 		}
 
-        T &operator[](std::size_t index) {
+		T &operator[](std::size_t index) {
 			return _arr[index];
-        }
+		}
 
 		T &at( std::size_t index ) {
 			if (index < _size)
@@ -105,23 +105,23 @@ namespace ft {
 				pop_back();
 		}
 
-        void push_back(T new_el){
-            if (_size + 1 >= _capacity)
+		void push_back(T new_el){
+			if (_size + 1 >= _capacity)
 				reserve(_capacity << 1);
-            _allocator.construct(&_arr[_size++], new_el);
-        }
+			_allocator.construct(&_arr[_size++], new_el);
+		}
 
-        void pop_back(){
+		void pop_back(){
 			_size--;
 			_allocator.destroy(&_arr[_size]);
-        }
+		}
 
-        std::size_t size() {
-            return (_size);
-        }
-        std::size_t capacity() {
-            return (_capacity);
-        }
+		std::size_t size() {
+			return (_size);
+		}
+		std::size_t capacity() {
+			return (_capacity);
+		}
 
 		void swap(vector& other) {
 			T *arr = _arr;
