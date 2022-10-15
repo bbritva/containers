4d0549eccb8e09536aab0e3faae08f761a499997
grvelva <grvelva@student.42.fr>
2022-10-15 17:29:45 +0200

    iterators in separated file

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@
 //#define NAMESPACE std
 #include "vector.hpp"
 #include "stack.hpp"
+#include "containers_test/srcs/base.hpp"
 
 #define COUNT 100
 
-#define TESTED_NAMESPACE NAMESPACE
-
 #define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
-#define TESTED_TYPE int
 
 template <typename T>
 void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
@@ -37,37 +35,6 @@ void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true
 			std::cout << "- " << *it << std::endl;
 	}
 	std::cout << "###############################################" << std::endl;
-}
-
-void	prepost_incdec(TESTED_NAMESPACE::vector<TESTED_TYPE> &vct)
-{
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it_tmp;
-
-	std::cout << "Pre inc" << std::endl;
-	it_tmp = ++it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Pre dec" << std::endl;
-	it_tmp = --it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Post inc" << std::endl;
-	it_tmp = it++;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Post dec" << std::endl;
-	it_tmp = it--;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-	std::cout << "###############################################" << std::endl;
-}
-
-void	checkErase(TESTED_NAMESPACE::vector<TESTED_TYPE> const &vct,
-				   TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator const &it)
-{
-	static int i = 0;
-	std::cout << "[" << i++ << "] " << "erase: " << it - vct.begin() << std::endl;
-	printSize(vct);
 }
 
 int main() {
@@ -160,32 +127,46 @@ int main() {
 	std::cout << std::endl;
 	 */
 	{
+#define TESTED_TYPE foo<int>
+
 		const int size = 5;
 		TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-		TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
-		TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator ite = vct.begin();
+		TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it(vct.begin());
+		TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator ite(vct.end());
 
-		for (int i = 0; i < size; ++i)
-			it[i] = (size - i) * 5;
-		prepost_incdec(vct);
-
-		it = it + 5;
-		it = 1 + it;
-		it = it - 4;
-		std::cout << *(it += 2) << std::endl;
-		std::cout << *(it -= 1) << std::endl;
-
-		*(it -= 2) = 42;
-		*(it += 2) = 21;
-
-		std::cout << "const_ite +=: " << *(ite += 2) << std::endl;
-		std::cout << "const_ite -=: " << *(ite -= 2) << std::endl;
-
-		std::cout << "(it == const_it): " << (ite == it) << std::endl;
-		std::cout << "(const_ite - it): " << (ite - it) << std::endl;
-		std::cout << "(ite + 3 == it): " << (ite + 3 == it) << std::endl;
-
+		for (int i = 1; it != ite; ++i)
+			*it++ = i;
 		printSize(vct, true);
+
+		it = vct.begin();
+		ite = vct.begin();
+
+		std::cout << *(++ite) << std::endl;
+		std::cout << *(ite++) << std::endl;
+		std::cout << *ite++ << std::endl;
+		std::cout << *++ite << std::endl;
+
+		it->m();
+		ite->m();
+
+		std::cout << *(++it) << std::endl;
+		std::cout << *(it++) << std::endl;
+		std::cout << *it++ << std::endl;
+		std::cout << *++it << std::endl;
+
+		std::cout << *(--ite) << std::endl;
+		std::cout << *(ite--) << std::endl;
+		std::cout << *--ite << std::endl;
+		std::cout << *ite-- << std::endl;
+
+		(*it).m();
+		(*ite).m();
+
+		std::cout << *(--it) << std::endl;
+		std::cout << *(it--) << std::endl;
+		std::cout << *it-- << std::endl;
+		std::cout << *--it << std::endl;
+
 	}
 	return (0);
 }
